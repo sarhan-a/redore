642975d12bd60981cf6af704e6b76ff4f41f3d8c
Sarhan Alim <sarhan.alim@gmail.com>
2022-08-18 21:43:34 +0100

    fix an error in listing model associations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,6 @@
 class Listing < ApplicationRecord
   belongs_to :user
-  has_many :reviews, :bookings
+  has_many :reviews
+  has_many :bookings
   validates_presence_of :title, :address, :details, :price
 end
