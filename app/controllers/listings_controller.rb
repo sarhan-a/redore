39f0bd008f348db525bddbd4539371fe01e5410c
jacopo belli <jacopo.b2016@gmail.com>
2022-08-20 16:01:34 +0100

    edit method

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -8,7 +8,7 @@ class ListingsController < ApplicationController
   def show
     @listing = Listing.find(params[:id])
   end
-  
+
   def new
     @listing = Listing.new
   end
@@ -19,6 +19,16 @@ class ListingsController < ApplicationController
     if @listing.save
       redirect_to listing_path(@listing)
     end
+  end
+
+  def edit
+    @listing = Listing.find(params[:id])
+  end
+
+  def update
+    @listing = Listing.find(params[:id])
+    @listing = Listing.update(listing_params)
+    redirect_to listing_path(@listing)
   end
 
   private
