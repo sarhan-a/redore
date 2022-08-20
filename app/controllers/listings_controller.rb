8b5d78033b4057619609a2349bb411444c3ff5f6
jacopo belli <jacopo.b2016@gmail.com>
2022-08-20 11:45:01 +0100

    routes edited, link to listing on index page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,5 @@
 class ListingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @listings = Listing.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :listings do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create, :show, :index]
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
