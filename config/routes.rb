14e73aa0f05782436bb57a02aafe2d5132f477eb
jacopo belli <jacopo.b2016@gmail.com>
2022-08-20 11:31:34 +0100

    routes done, view and action method genereated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :listings do
+    resources :bookings, only: [:new, :create]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
