220fa1300ae82c831c1917a560f3711bb383ce4c
Sarhan Alim <sarhan.alim@gmail.com>
2022-08-21 01:36:31 +0100

    review view partial and average rating method

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,4 +6,12 @@ class Listing < ApplicationRecord
 
   validates_presence_of :title, :address, :details, :price
   validates :price, numericality: true
+
+  def average_rating
+    if reviews.size.positive?
+      reviews.average(:rating)
+    else
+      'No ratings yet'
+    end
+  end
 end
