73ba45917af62d30baca094cb3aa06d913ed0e3d
Taylan Ozdere <taylan@ozdere.net>
2022-08-23 18:36:24 +0100

    remove CollectionDate views

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,4 +2,3 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
-import flatpickr from "flatpickr"
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,4 +6,7 @@ const application = Application.start()
 application.debug = false
 window.Stimulus   = application
 
+import Flatpickr from 'stimulus-flatpickr'
+application.register('flatpickr', Flatpickr)
+
 export { application }
