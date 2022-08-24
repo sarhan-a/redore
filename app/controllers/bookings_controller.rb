3825c6f2f1954d922d656a07c658be0e8f1265aa
Taylan Ozdere <taylan@ozdere.net>
2022-08-24 14:12:33 +0100

    disable flatpickr dates for already booked dates

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,6 +2,8 @@ class BookingsController < ApplicationController
   def new
     @listing = Listing.find(params[:listing_id])
     @booking = @listing.bookings.new(user: current_user)
+    @bookings = Booking.where('collection_date >= ? AND collection_date < ?', Time.current, Time.current + 90.days).order(collection_date: :asc)
+    @booked_dates = @bookings.pluck(:collection_date)
   end
 
   def create
