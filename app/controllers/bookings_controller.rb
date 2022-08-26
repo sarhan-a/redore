837732c0027328927248a06e03dc83b07e5767e1
Taylan Ozdere <taylan@ozdere.net>
2022-08-26 18:46:19 +0100

    fix flatpickr disabling dates on all listing instances

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,7 +2,7 @@ class BookingsController < ApplicationController
   def new
     @listing = Listing.find(params[:listing_id])
     @booking = @listing.bookings.new(user: current_user)
-    @bookings = Booking.where('collection_date >= ? AND collection_date < ?', Time.current, Time.current + 90.days).order(collection_date: :asc)
+    @bookings = @listing.bookings.where('collection_date >= ? AND collection_date < ?', Time.current, Time.current + 90.days).order(collection_date: :asc)
     @booked_dates = @bookings.pluck(:collection_date)
   end
 
