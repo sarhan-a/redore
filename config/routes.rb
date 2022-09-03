b75a380bd0619867768492c417c52422bd9d9910
jacopo belli <jacopo.b2016@gmail.com>
2022-09-03 10:47:49 +0100

    user's bookings showpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users
   root to: "pages#home"
   resources :listings do
     resources :bookings, only: %i[new create show index]
