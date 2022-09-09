611426567a43fda493e0274dab9c7323375d096a
Sarhan Alim <sarhan.alim@gmail.com>
2022-09-09 16:37:00 +0100

    fix cloudinary avatar bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def user_avatar(user)
     if user.avatar.attached?
-      user.avatar
+      cl_image_path user.avatar.key, crop: :fill, gravity: :face, width: 200, height: 200
     else
       gravatar_image_url(user.email)
     end
