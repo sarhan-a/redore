0cb4390881c3afde3d34e50657968188a6df9eef
Sarhan Alim <sarhan.alim@gmail.com>
2022-09-19 21:16:52 +0100

    add more styling and components to listing show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def user_avatar(user)
     if user.avatar.attached?
-      cl_image_path user.avatar.key, crop: :fill, gravity: :face, width: 200, height: 200
+      cl_image_path user.avatar.key, crop: :fill, gravity: :face, width: 250, height: 250
     else
       gravatar_image_url(user.email)
     end
