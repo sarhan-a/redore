3e7b3f4369f51247f32d3a2bf96eda7a677c0095
Mohed Nazari <mohed.nazari@gmail.com>
2022-09-23 19:25:50 +0100

    chaned my profile picture in the about us team section

--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_23_143626) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "conversations", force: :cascade do |t|
+    t.integer "recipient_id"
+    t.integer "sender_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipient_id", "sender_id"], name: "index_conversations_on_recipient_id_and_sender_id", unique: true
+    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
+  end
+
   create_table "favorites", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "favorited_type", null: false
@@ -73,6 +83,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_23_143626) do
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.bigint "conversation_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -107,6 +127,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_23_143626) do
   add_foreign_key "bookings", "users"
   add_foreign_key "favorites", "users"
   add_foreign_key "listings", "users"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
   add_foreign_key "reviews", "listings"
   add_foreign_key "reviews", "users"
 end
