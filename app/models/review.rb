a14b0e7aeeaa3f9d204569e8b2b4d45b50e36175
Sarhan Alim <sarhan.alim@gmail.com>
2022-09-24 10:59:36 +0100

    reviews sorted by newest by default

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,5 @@ class Review < ApplicationRecord
   validates :rating, presence: true
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
   validates :content, presence: true, length: { minimum: 5 }
+  default_scope { order('created_at DESC') }
 end
